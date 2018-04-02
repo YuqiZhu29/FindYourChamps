daa7f8c6fc4b73c3a06a78eb092136828edb77a4
lostloving01 <loutsoul10@gmail.com>
2018-04-01 21:12:12 -0400

    Construct Static Champs Data

---
+++ src/main/java/org/EPHProj/FindYourChamps/model/LOL_STATIC_DATA_V3.java
@@ -0,0 +1,38 @@
+package org.EPHProj.FindYourChamps.model;
+
+public class LOL_STATIC_DATA_V3 {
+
+    //property
+    private String name;
+    private int id;
+    private String key;
+
+    //constructor
+    public LOL_STATIC_DATA_V3 (String name, int id, String key) {
+        this.name=name;
+        this.id=id;
+        this.key=key;
+    }
+
+    // get/set method
+    public int get_id () {
+        return id;
+    }
+    public void set_id (int id_) {
+        id=id_;
+    }
+
+    public String get_name () {
+        return name;
+    }
+    public void set_name (String name_) {
+        name=name_;
+    }
+
+    public String get_key () {
+        return key;
+    }
+    public void set_key (String key_) {
+        key=key_;
+    }
+}
